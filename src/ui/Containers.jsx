a5b2b8ffc681ae88f89963b260f88d1cec70a17a
William Franklin <emailwilliamfranklin@gmail.com>
2024-09-09 12:00:57 -0700

    fix: tweaks to useRef

--- src/ui/Containers.jsx
+++ src/ui/Containers.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import './Containers.css'
 
 
@@ -6,22 +6,33 @@ import './Containers.css'
 function Sidebar() {
 
     function Wrapper() {
-        let testState = useState(false)
+        let testState = useRef(false)
+        const targDiv = useRef(null)
 
         function updateState() {
             console.log(testState)
             if (testState === false) {
                 testState = true
+                updateStyle(targDiv, testState)
             } else {
                 testState = false
+                updateStyle(targDiv, testState)
             }
-            console.log(testState)
+        }
+
+        function updateStyle(targ, state) {
+            console.log(targ.current.classList)
+            if (targ && state === true) {
+                targ.current.classList.add('clicked')
+            } else {
+                targ.current.classList.remove('clicked')
+            }
         }
         return (
             <>
                 <div id='sb-wrapper'>
                     <div id='sb-header'>
-                        <div id='test-button' onClick={updateState}></div>
+                        <div id='test-button' ref={targDiv} onClick={updateState}></div>
                     </div>
                     <div id='sb-body'></div>
                     <div id='sb-footer'></div>
